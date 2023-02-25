9ece23d5f4849a7e76d9d47b639ece7a20f931de
marzhanm <124434547+marzhanm@users.noreply.github.com>
2023-02-25 23:35:19 +0600

    Update MovableCircle.java

---
+++ problem-A/MovableCircle.java
@@ -0,0 +1,33 @@
+class MovableCircle implements MovablePoint{
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+    public String toString() {
+        return String.format("%s, radius=%d", center.toString(), radius);
+    }
+
+}
