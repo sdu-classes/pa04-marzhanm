4b5b0b3ccb6eed5141116b85f6f9f8ab85d67532
marzhanm <124434547+marzhanm@users.noreply.github.com>
2023-02-25 23:58:48 +0600

    Update Spider.java

---
+++ problem-B/Spider.java
@@ -0,0 +1,8 @@
+class Spider extends Animal {
+    Spider() {
+        super(8);
+    }
+    void eat() {
+        System.out.println("Aspider'sdiet consists mainly of mosquitoes, flies, moths, and sometimes even other spiders.");
+    }
+}
